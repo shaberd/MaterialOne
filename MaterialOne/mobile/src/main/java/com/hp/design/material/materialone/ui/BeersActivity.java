3f8cb49132aaa4802685f36824525bc32f9bd979
shaberd <david.shaber@hp.com>
2016-03-18 21:06:18 -0600

    copy notes to master fix beers activity to point to motion activity and use plus icon

--- MaterialOne/mobile/src/main/java/com/hp/design/material/materialone/ui/BeersActivity.java
+++ MaterialOne/mobile/src/main/java/com/hp/design/material/materialone/ui/BeersActivity.java
@@ -1,5 +1,6 @@
 package com.hp.design.material.materialone.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -79,8 +80,7 @@ public class BeersActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(getApplicationContext(), MotionActivity.class));
             }
         });
 
